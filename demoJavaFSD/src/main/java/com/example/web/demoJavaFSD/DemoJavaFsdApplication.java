903e765d832fbbbca38332353819d52dd3946ec2
sankaranarayanan386 <sankaranarayanan386@gmail.com>
2019-09-04 05:56:22 -0700

    Sprinig INIT Version

---
+++ demoJavaFSD/src/main/java/com/example/web/demoJavaFSD/DemoJavaFsdApplication.java
@@ -0,0 +1,13 @@
+package com.example.web.demoJavaFSD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaFsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaFsdApplication.class, args);
+	}
+
+}
---
+++ demoJavaFSD/src/test/java/com/example/web/demoJavaFSD/DemoJavaFsdApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.web.demoJavaFSD;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoJavaFsdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
